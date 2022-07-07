6b07a0e67b679e6ede2c93158fcb20955b419a91
Andrianna <andrianna.odell@gmail.com>
2022-07-07 18:14:12 -0400

    commit tree 56a459cb1cf48a2d5321d8b80c396da70a0074db

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+// find which names are matching
+
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+
+function findMatching(drivers, names){
+    const result = drivers.filter(names => drivers.tolowerCase() === names.toLowerCase());
+    return console.log(result);
+}
+
+function fuzzyMatch() {
+
+}
+
+function matchName() {
+
+}
